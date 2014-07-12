4b3392b75c21fe81f36e4e47bcb4920dc974c8d2
Josh <joshpadilla@gmail.com>
2014-07-12 13:44:02 -0700

    Add sample OAuth stanzas

--- addon.py
+++ addon.py
@@ -9,3 +9,32 @@ line1 = "This is a simple example of notifications"
 time = 5000  #in miliseconds
  
 xbmc.executebuiltin('Notification(%s, %s, %d, %s)'%(__addonname__,line1, time, __icon__))
+
+# import oauth2 as oauth
+# import urlparse 
+# 
+# consumer_key           = "CONSUMER_KEY"
+# consumer_secret        = "CONSUMER_SECRET"
+# consumer = oauth.Consumer(consumer_key, consumer_secret)
+# client = oauth.Client(consumer)
+#
+#
+# request_token_url      = 'https://api.linkedin.com/uas/oauth/requestToken'
+# resp, content = client.request(request_token_url, "POST")
+# if resp['status'] != '200':
+#    raise Exception("Invalid response %s." % resp['status'])
+# 
+# request_token = dict(urlparse.parse_qsl(content))
+#
+#
+# print "Request Token:"
+# print "    - oauth_token        = %s" % request_token['oauth_token']
+# print "    - oauth_token_secret = %s" % request_token['oauth_token_secret']
+# print 
+#
+#
+# Provider redirect
+# authorize_url =      'https://api.linkedin.com/uas/oauth/authorize'
+# print "Go to the following link in your browser:"
+# print "%s?oauth_token=%s" % (authorize_url, request_token['oauth_token'])
+# print 
