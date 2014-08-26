ce89d86270d022e1afefad27bec094e930af7875
Josh <joshpadilla@gmail.com>
2014-08-26 12:17:41 -0700

    Notify txt change

--- addon.py
+++ addon.py
@@ -7,7 +7,7 @@ __addon__       = xbmcaddon.Addon()
 __addonname__   = __addon__.getAddonInfo('name')
 __icon__        = __addon__.getAddonInfo('icon')
  
-line1 = "This is a simple example of notifications"
+line1 = "Simple notification example"
 time = 5000  #in miliseconds
  
 xbmc.executebuiltin('Notification(%s, %s, %d, %s)'%(__addonname__,line1, time, __icon__)) 
